b1400090ec296dd993348341a44089f7ac08fcdf
Abbas Sayed <imabbassayed@gmail.com>
2022-11-03 18:34:15 +0400

    Working on header

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {
+  Button,
+} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
@@ -17,14 +20,28 @@ function App(){
   return (
     <NavigationContainer>
 
-      <stack.Navigator screenOptions={{headerShown: false}} >
-        <stack.Group>
+      <stack.Navigator>
+        <stack.Group screenOptions={{headerShown: false}} >
               <stack.Screen name="Login" component={LoginScreen} />
               <stack.Screen name="Register" component={RegisterScreen} />
         </stack.Group>  
 
         <stack.Group>
-            <stack.Screen name="Home" component={HomeScreen} />            
+            <stack.Screen 
+            name="Home" 
+            component={HomeScreen}
+            options={{
+              headerBackVisible : false,
+              headerTitle : "",
+              headerRight : () => (
+                <Button
+                  onPress={() => alert('This is a button!')}
+                  title="Info"
+
+                />
+              ),
+
+            }} />            
         </stack.Group>
       </stack.Navigator>
     
