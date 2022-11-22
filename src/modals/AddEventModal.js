ce26fb51cbae403cb8a697cc489983ae8d9e5cff
Abbas Sayed <imabbassayed@gmail.com>
2022-11-22 17:19:43 +0000

    Added add event fields and installed datetimepicker

--- src/modals/AddEventModal.js
+++ src/modals/AddEventModal.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import React, {useState} from 'react';
+import { View, Text, Switch } from 'react-native';
 
 import  Modal  from 'react-native-modal';
 import InputField from '../components/InputField';
@@ -7,8 +7,10 @@ import InputField from '../components/InputField';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 
-
 const AddEventModal = ({navigation}) => {
+
+const [isAllDayEnabled, setIsAllDayEnabled] = useState(false);
+const toggleSwitch = () => setIsAllDayEnabled(previousState => !previousState);
 
   return(
     <View>
@@ -31,7 +33,7 @@ const AddEventModal = ({navigation}) => {
                 marginBottom: 12,
                 fontSize: 20,
                 fontWeight: '500',
-                color: '#333',
+                color: '#AD40AF',
             }}>Add Event</Text>
             <InputField
                 label={'Title *'}
@@ -43,9 +45,7 @@ const AddEventModal = ({navigation}) => {
                     style={{marginRight: 5}}
                 />
                 }
-                style= {{
-                    width: 5
-                }}
+                
                 inputType="Text"
                 text={text => setTitle(text)}
             />
@@ -60,20 +60,27 @@ const AddEventModal = ({navigation}) => {
                     style={{marginRight: 5}}
                 />
                 }
-                style= {{
-                    width: 5
-                }}
+                
                 inputType="Text"
-                text={text => setTitle(text)}
+                text={text => setLocation(text)}
             />
-            Allday toggloe
-            Starts datepicker
-            end datecpicker
-            repeat select
+            <View style={{flexDirection:'row'}}>
+                <Text style={{
+                    fontSize: 15,
+                    fontWeight: '500',
+                    color: '#AD40AF',
+                    right: 50
+                }}>All Day</Text>
+                <Switch  
+                onChange={toggleSwitch}
+                value={isAllDayEnabled}
+                trackColor={{true: '#AD40AF'}}/>
+            </View>
+
+            
+
+         
         </View>
-
-       
-
     </Modal>
 
     </View>
