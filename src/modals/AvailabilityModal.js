5d44ef8e057041e7522a5f1d14c64d60a0305367
Abbas Sayed <imabbassayed@gmail.com>
2023-03-08 19:31:38 +0000

    Web Slot Booking Implementation

--- src/modals/AvailabilityModal.js
+++ src/modals/AvailabilityModal.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput, Alert } from 'react-native';
 
 import  Modal  from 'react-native-modal';
 
@@ -8,7 +8,7 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 import DateTimePicker from '@react-native-community/datetimepicker';
 
 
-import { collection, query, where, getDocs } from "firebase/firestore";
+import { collection, query, where, getDocs, addDoc } from "firebase/firestore";
 import { userId, db } from '../../firebaseConfig';
 
 import * as Clipboard from 'expo-clipboard';
@@ -20,6 +20,8 @@ const AvailabilityModal = (props) => {
 const [fromTime, setFromTime] = useState(new Date());
 const [toTime, setToTime] = useState(new Date());
 const [date, setDate] = useState(new Date());
+const [duration, setDuration] = useState(0);
+
 
 const availability = []
 
@@ -42,7 +44,37 @@ const copyToClipboard = async () => {
   };
 
 
-const fetchAvailability = async () => {
+  const generateSlotBookingToken = async () => {
+
+    const availabilityFLattened = []
+    availability.map( function(value, key){
+      availabilityFLattened.push(value[0])
+      availabilityFLattened.push(value[1])
+    })
+    
+    try {
+        const docRef = await addDoc(collection(db, "slotbookingtokens"), {
+          date : date,
+          duration : duration,
+          availability : availabilityFLattened,
+          user: userId,
+        });
+
+        await Clipboard.setStringAsync('http://localhost:3000/index.php?id='+docRef.id);
+        
+      } catch (e) {
+        console.log(e)
+        Alert.alert(
+          "Insert Failed !",
+          "Please try again",
+        );
+      }
+
+
+  };
+
+
+const fetchAvailability = async (type) => {
     availability.push([fromTime.getTime(),toTime.getTime()])
 
     date.setHours(0)
@@ -74,10 +106,17 @@ const fetchAvailability = async () => {
             
     });
 
-    copyToClipboard()
+    availability.map( function(value, key){
+      if (value[1] - value[0] < duration){
+          delete availability[key]
+      }
+    });
 
-
-    
+    if (type == 1){
+        copyToClipboard();
+    }else if (type == 2){
+        generateSlotBookingToken();
+    }
 
     }
 
@@ -186,6 +225,45 @@ const fetchAvailability = async () => {
             onChange = {(_,to) => setToTime(to)}
             />
 
+            <Text style={{
+                                fontSize: 20,
+                                fontWeight: '500',
+                                color: '#AD40AF',
+                                marginTop: 20,
+                                
+                            }}>Duration</Text>
+
+            <View
+              style={{
+                flexDirection: 'row',
+                borderBottomColor: '#ccc',
+                borderBottomWidth: 1,
+                paddingBottom: 8,
+                marginBottom: 50,
+                width:200
+              }}>
+
+              <Ionicons
+                    name="timer-outline"
+                    size={40}
+                    color="#666"
+                    style = {{
+                      marginRight : 5
+                    }}
+                />
+            <TextInput
+              placeholder= "Duration *"
+              keyboardType="numeric"
+              style={{flex: 1}}
+              onChangeText = {(text) => {
+                setDuration(text)
+              }}
+            />
+
+             
+
+              </View>
+
                 <View style={{flexDirection:'row'}}>
                 <TouchableOpacity
                 onPress={props.close}
@@ -196,7 +274,7 @@ const fetchAvailability = async () => {
                     justifyContent: 'center',
                     borderRadius : 50,
                     backgroundColor: '#666',
-                    right: 115
+                    right: 100
                     
                     
                   }}             
@@ -208,7 +286,24 @@ const fetchAvailability = async () => {
                 </TouchableOpacity>
 
                 <TouchableOpacity
-                onPress={() => {fetchAvailability()}}
+                onPress={() => {fetchAvailability(1)}}
+                style={{
+                    width: 50,
+                    height: 50,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    borderRadius : 50,
+                    backgroundColor: '#AD40AF',
+                }}             
+                > 
+                    <View>
+                    <Ionicons name="copy-outline" size="30" color="white" />          
+                    </View>
+                
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                onPress={() => {fetchAvailability(2)}}
                 style={{
                     width: 60,
                     height: 60,
@@ -216,14 +311,15 @@ const fetchAvailability = async () => {
                     justifyContent: 'center',
                     borderRadius : 50,
                     backgroundColor: '#AD40AF',
-                    left: 115
+                    left: 100
                 }}             
                 > 
                     <View>
-                    <Ionicons name="copy-outline" size="40" color="white" />          
+                    <Ionicons name="file-tray-stacked-outline" size="40" color="white" />          
                     </View>
                 
                 </TouchableOpacity>
+
                 </View>
                 
 
